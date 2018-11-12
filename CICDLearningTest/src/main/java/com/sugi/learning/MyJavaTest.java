e04aea395d955771e22956af8679ad22bba952a4
sugi <sugithanandan@gmail.com>
2018-11-12 13:17:34 +0530

    JavaTest class commit

---
+++ CICDLearningTest/src/main/java/com/sugi/learning/MyJavaTest.java
@@ -0,0 +1,16 @@
+package com.sugi.learning;
+
+public class MyJavaTest {
+	
+	
+	public MyJavaTest()
+	{
+		System.out.println("Hi I am constructor");
+	}
+	
+	public void displayTest()
+	{
+		System.out.println("Hi How are you!!!");
+	}
+
+}
